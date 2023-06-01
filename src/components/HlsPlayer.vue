51ce4afc57c1f733e862414c3ccfb53e0a506f38
Alex <alex_sizow@outlook.com>
2023-06-01 18:46:02 +0300

    template player code

---
+++ src/components/HlsPlayer.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+import Hls from 'hls.js';
+
+defineProps<{ videoUrl: string }>();
+
+onMounted(() => {
+	let hls = new Hls();
+	let stream = '../assets/master.m3u8';
+	let video = ref(null);
+	hls.loadSource(stream);
+	hls.attachMedia(video);
+	hls.on(Hls.Events.MANIFEST_PARSED, function () {
+		video.play();
+	});
+});
+</script>
+
+<template>
+	<video
+		ref="video"
+		controls
+		src=""></video>
+</template>
+
+<style scoped>
+.read-the-docs {
+	color: #888;
+}
+</style>
